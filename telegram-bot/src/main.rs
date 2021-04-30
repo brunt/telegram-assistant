2cd5b31267613b3998e5055c9462891983b443fe
Bryant Deters <bryantdeters@gmail.com>
2021-04-29 22:15:57 -0500

    teloxide 3 -> 4, compiles but not compatible with the reactor from actix 3, and actix 4 is not compatible with latest actix-web-prom

--- telegram-bot/src/main.rs
+++ telegram-bot/src/main.rs
@@ -2,6 +2,7 @@ use actix_web::{App, HttpServer};
 use actix_web_prom::PrometheusMetrics;
 use prometheus::{opts, IntCounterVec};
 use teloxide::prelude::*;
+use tokio_stream::wrappers::UnboundedReceiverStream;
 
 mod config;
 mod dispatch;
@@ -12,33 +13,34 @@ mod spending;
 use config::Config;
 use dispatch::parse_messages;
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() {
-    let prometheus = PrometheusMetrics::new("teloxide", Some("/metrics"), None);
-    let counter_opts = opts!("counter", "requests").namespace("teloxide");
-    let counter = IntCounterVec::new(counter_opts, &["request"]).unwrap();
-    prometheus
-        .registry
-        .register(Box::new(counter.clone()))
-        .unwrap();
+    // let prometheus = PrometheusMetrics::new("teloxide", Some("/metrics"), None);
+    // let counter_opts = opts!("counter", "requests").namespace("teloxide");
+    // let counter = IntCounterVec::new(counter_opts, &["request"]).unwrap();
+    // prometheus
+    //     .registry
+    //     .register(Box::new(counter.clone()))
+    //     .unwrap();
     let config = Config::from_env();
-    run_webserver(&config, prometheus);
-    run_chatbot(config, counter).await;
+    // run_webserver(&config, prometheus); //TODO: enable when actix-web 4 and actix-web-prom are compatible
+    run_chatbot(config, /*counter*/).await;
 }
 
-fn run_webserver(config: &Config, prometheus: PrometheusMetrics) {
-    HttpServer::new(move || App::new().wrap(prometheus.clone()))
-        .bind(format!("0.0.0.0:{}", &config.webserver_port))
-        .expect("address in use")
-        .run();
-}
+// fn run_webserver(config: &Config, prometheus: PrometheusMetrics) {
+//     HttpServer::new(move || App::new().wrap(prometheus.clone()))
+//         .bind(format!("0.0.0.0:{}", &config.webserver_port))
+//         .expect("address in use")
+//         .run();
+// }
 
-async fn run_chatbot(config: Config, counter: IntCounterVec) {
-    let bot = Bot::from_env();
+async fn run_chatbot(config: Config /*, counter: IntCounterVec*/) {
+    let bot = Bot::from_env().auto_send();
     Dispatcher::new(bot)
-        .messages_handler(move |rx: DispatcherHandlerRx<Message>| {
-            rx.for_each_concurrent(None, move |msg| {
-                parse_messages(msg, config.clone(), counter.clone())
+        .messages_handler(move |rx: DispatcherHandlerRx<AutoSend<Bot>, Message>| {
+            UnboundedReceiverStream::new(rx)
+            .for_each_concurrent(None, move |msg| {
+                parse_messages(msg, config.clone()/*, counter.clone()*/)
             })
         })
         .dispatch()
