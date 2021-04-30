41dc08576cf3040eb15b8838040610d8dd2f040a
Bryant Deters <bryantdeters@gmail.com>
2021-04-29 22:15:57 -0500

    teloxide 3 -> 4, compiles but not compatible with the reactor from actix 3, and actix 4 is not compatible with latest actix-web-prom

--- telegram-bot/src/dispatch.rs
+++ telegram-bot/src/dispatch.rs
@@ -2,7 +2,7 @@ use crate::config::Config;
 use crate::metro::{help_schedule, is_next_arrival_request};
 use crate::spending::{help_spending, is_spent_category_request, is_spent_request};
 use metro_schedule::NextArrivalRequest;
-use prometheus::IntCounterVec;
+// use prometheus::IntCounterVec;
 use teloxide::prelude::*;
 
 fn helpmsg() -> &'static str {
@@ -10,22 +10,22 @@ fn helpmsg() -> &'static str {
 }
 
 pub(crate) async fn parse_messages(
-    msg: UpdateWithCx<Message>,
+    msg: UpdateWithCx<AutoSend<Bot>, Message>,
     config: Config,
-    counter: IntCounterVec,
+    /*counter: IntCounterVec,*/
 ) {
     if let Some(txt) = msg.update.text() {
         if txt.eq("Help") {
-            counter.with_label_values(&["Help"]).inc();
-            msg.answer_str(helpmsg()).await.unwrap();
+            // counter.with_label_values(&["Help"]).inc();
+            msg.answer(helpmsg()).await.unwrap();
         } else if txt.eq("Help schedule") {
-            counter.with_label_values(&["Help schedule"]).inc();
-            msg.answer_str(help_schedule()).await.unwrap();
+            // counter.with_label_values(&["Help schedule"]).inc();
+            msg.answer(help_schedule()).await.unwrap();
         } else if txt.eq("Help spending") {
-            counter.with_label_values(&["Help spending"]).inc();
-            msg.answer_str(help_spending()).await.unwrap();
+            // counter.with_label_values(&["Help spending"]).inc();
+            msg.answer(help_spending()).await.unwrap();
         } else if is_next_arrival_request(txt) {
-            counter.with_label_values(&["Next Arrival"]).inc();
+            // counter.with_label_values(&["Next Arrival"]).inc();
             let data_vec: Vec<&str> = txt.splitn(2, ' ').collect();
             match &config
                 .metro_api
@@ -36,18 +36,18 @@ pub(crate) async fn parse_messages(
                 .await
             {
                 Ok(s) => {
-                    msg.answer_str(s.to_string()).await.unwrap();
+                    msg.answer(s.to_string()).await.unwrap();
                 }
                 Err(_) => {
-                    msg.answer_str("An error occurred retrieving the schedule")
+                    msg.answer("An error occurred retrieving the schedule")
                         .await
                         .unwrap();
                 }
             }
         } else if is_spent_category_request(txt) {
-            counter.with_label_values(&["Spending"]).inc();
+            // counter.with_label_values(&["Spending"]).inc();
             let category: &str = txt.splitn(3, ' ').last().unwrap();
-            msg.answer_str(
+            msg.answer(
                 &config
                     .spending_api
                     .parse_spent_request(txt, Some(category.into()))
@@ -56,8 +56,8 @@ pub(crate) async fn parse_messages(
             .await
             .unwrap();
         } else if is_spent_request(txt) {
-            counter.with_label_values(&["Spending"]).inc();
-            msg.answer_str(&config.spending_api.parse_spent_request(txt, None).await)
+            // counter.with_label_values(&["Spending"]).inc();
+            msg.answer(&config.spending_api.parse_spent_request(txt, None).await)
                 .await
                 .unwrap();
         }
