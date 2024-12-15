549c23ee5c93123fd474baff38c594672b05eadf
brunt <bryantdeters@gmail.com>
2024-12-15 10:36:52 -0600

    finished api portion

--- notification-service/src/main.rs
+++ notification-service/src/main.rs
@@ -1,6 +1,6 @@
 use axum::{
     extract::State,
-    routing::{get, post},
+    routing::{get, post, delete},
     Json, Router,
 };
 use chrono::{DateTime, Duration, Utc};
@@ -17,6 +17,16 @@ struct Notification {
     created_at: String,
 }
 
+impl Default for Notification {
+    fn default() -> Self {
+        Self {
+            id: Uuid::new_v4().to_string(),
+            message: "Notification Serviced started.".to_string(),
+            created_at: Utc::now().to_string(),
+        }
+    }
+}
+
 #[derive(Clone)]
 struct AppState {
     notifications: Arc<RwLock<Vec<Notification>>>,
@@ -24,7 +34,7 @@ struct AppState {
 
 async fn create_notification(
     State(state): State<Arc<AppState>>,
-    Json(payload): Json<String>,
+    payload: String,
 ) -> Json<Notification> {
     let notification = Notification {
         id: Uuid::new_v4().to_string(),
@@ -35,7 +45,7 @@ async fn create_notification(
     state
         .notifications
         .write()
-        .unwrap()
+        .expect("failed to obtain write lock during create")
         .push(notification.clone());
 
     Json(notification)
@@ -45,7 +55,7 @@ async fn get_notifications(State(state): State<Arc<AppState>>) -> Json<Vec<Notif
     let notifications = state
         .notifications
         .read()
-        .expect("failed to obtain rwlock for notifications")
+        .expect("failed to obtain read lock when reading notifications")
         .clone();
     Json(notifications)
 }
@@ -58,9 +68,25 @@ async fn cleanup_old_notifications(state: Arc<AppState>) {
         state
             .notifications
             .write()
-            .expect("failed to write to rwlock")
+            .expect("failed to write to get write lock during scheduled cleanup")
             .retain(|n| n.created_at.parse::<DateTime<Utc>>().unwrap_or(Utc::now()) > week_ago);
     }
+}
+
+async fn clear_notifications(State(state): State<Arc<AppState>>) -> Json<Vec<Notification>> {
+    state
+        .notifications
+        .write()
+        .expect("failed to get write lock when clearing notifications")
+        .clear();
+
+    Json(
+        state
+            .notifications
+            .read()
+            .expect("failed to read notifications after clearing them")
+            .clone(),
+    )
 }
 
 #[tokio::main]
@@ -68,11 +94,11 @@ async fn main() {
     let cmd = command!()
         .arg(arg!( -p --port [port] "port number for webserver").required(false))
         .get_matches();
-    let default_port = "8005".to_string();
+    let default_port = "8002".to_string();
     let port = cmd.get_one::<String>("port").unwrap_or(&default_port);
 
     let state = Arc::new(AppState {
-        notifications: Arc::new(RwLock::new(Vec::new())),
+        notifications: Arc::new(RwLock::new(vec![Notification::default()])),
     });
 
     let cleanup_state = state.clone();
@@ -84,10 +110,13 @@ async fn main() {
     let app = Router::new()
         .route("/notifications", post(create_notification))
         .route("/notifications", get(get_notifications))
+        .route("/notifications", delete(clear_notifications))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
         .await
         .expect("Failed to start webserver. Port already in use?");
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .expect("failed to start webserver");
 }
